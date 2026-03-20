b1ba6976fbdfed10571a7074f22b420f6b61127c
Rafael Hueb <rafaelhueb@192.168.1.7>
2026-03-20 18:51:48 -0300

    include metrics of terraform again

--- bash/correct-drift.sh
+++ bash/correct-drift.sh
@@ -1,3 +1,8 @@
+terraform init \
+        -backend-config="bucket=$TF_STATE_BUCKET" \
+        -backend-config="key=terraform/state.tfstate" \
+        -backend-config="region=$AWS_REGION" \
+        -backend-config="encrypt=true"
 terraform state rm aws_db_instance.poc
 awk '{sub(/engine_version      = "8.0"/, "engine_version      = \"8.4.7\""); print}' rds.tf > tmp && mv tmp rds.tf
 terraform import aws_db_instance.poc "poc-drift-mysql"
