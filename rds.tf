bf5818ce3b1b22567f8ebd67c2a987060a5e3002
Rafael Hueb <rafaelhueb@192.168.1.7>
2026-03-20 10:16:55 -0300

    engine rds version to 8.0

--- rds.tf
+++ rds.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "poc" {
   identifier          = "poc-drift-mysql"
   engine              = "mysql"
-  engine_version      = "8.4.7"        
+  engine_version      = "8.0"        
   instance_class      = "db.t3.micro"
   allocated_storage   = 20
   db_name             = "pocdb"
