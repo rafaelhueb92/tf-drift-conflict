b74c019ab028e979b7a089d490f19c050e44b45c
Rafael Hueb <rafaelhueb@192.168.1.7>
2026-03-19 22:27:54 -0300

    include destroy

--- bash/correct-drift.sh
+++ bash/correct-drift.sh
@@ -1,3 +1,3 @@
 terraform state rm aws_db_instance.poc
-awk '{sub(/engine_version      = "8.0"/, "engine_version      = \"8.4.7\""); print}' main.tf > tmp && mv tmp main.tf
+awk '{sub(/engine_version      = "8.0"/, "engine_version      = \"8.4.7\""); print}' rds.tf > tmp && mv tmp rds.tf
 terraform import aws_db_instance.poc "poc-drift-mysql"
